701ced3457d2e7f76aef812e15eed2f4f3a21a38
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-27 14:53:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   smallest_value = nil
   name_hash.each do
     |key, val|
-    if smallest_value == nil || key < smallest_value
+    if smallest_value == nil || val < smallest_value
       smallest_value = val
       smallest_value_key = key
     end
